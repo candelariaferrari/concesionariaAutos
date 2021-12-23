ed94fce355a8805cf8491d04ce62decec7290c8b
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2021-12-23 15:38:23 -0300

    tercera etapa

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 let autos = require('./autos')
 let concesionaria = {
-  listaAutos: autos,
+  autos: autos, //lista de autos
   buscarAuto: function (patente) {
     for (let i = 0; i < autos.length; i++) {
       if (autos[i].patente === patente) {
-        //let autoEncontrado = JSON.stringify(autos[i]);
+        /* asi funciona en el visual
+        let autoEncontrado = JSON.stringify(autos[i]); // devuelve todos los datos del auto
+        return autoEncontrado; */
+        /* asi funciona en el playground */
         let autoEncontrado = autos[i];
         return autoEncontrado;
-        //return JSON.stringify(autos[i]); (me pasaria todo el auto)
       }
     }
     return null;
-  }
+  },
+  venderAuto: function (patente) {
+    let auto = this.buscarAuto(patente);
+    if (auto === null) {
+      return console.log("Ya se vendio")
+    }
+    else {
+      return 'Marca: ' + auto.marca + ' - Modelo: ' + auto.modelo + ' - Auto vendido: ' + (auto.vendido = true) ;
+    }
+  },
+ /*  autosParaLaVenta: function(){
+    return autos = this.autos.filter(function (elemento){
+      return !elemento.vendido
+   })
+  } */
 }
-let resultado= concesionaria.buscarAuto('APL123')
-console.log("auto: " + resultado)
+let resultado = concesionaria.autosParaLaVenta(true)
+console.log(resultado)
